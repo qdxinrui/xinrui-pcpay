8f66f2cff901bd5a4f4fce3fe4ae207cb5d66ce4
tangxzh <tangxinzhang@126.com>
2019-01-11 11:39:57 +0800

    test

---
+++ src/Lite.php
@@ -0,0 +1,14 @@
+<?php
+namespace Qdxinrui\pcpay;
+class Lite
+{
+    /**
+     * 获取引擎
+     * @var string
+     */
+    private $engine;
+    public function getPayment()
+    {
+        echo '123';
+    }
+}
