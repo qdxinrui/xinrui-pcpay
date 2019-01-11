6255dbb6d2cc7a146c0e7873c1ff9ebc7e2c925c
tangxzh <tangxinzhang@126.com>
2019-01-11 18:12:13 +0800

    test

---
+++ src/weixin/lib/WxPay.Exception.php
@@ -0,0 +1,15 @@
+<?php
+namespace Qdxinrui\pcpay\weixin\lib;
+use Exception;
+/**
+ *
+ * 微信支付API异常类
+ * @author widyhu
+ *
+ */
+class WxPayException extends Exception {
+    public function errorMessage()
+    {
+        return $this->getMessage();
+    }
+}
